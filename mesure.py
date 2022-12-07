4d6ee88d7f5a1cd68ebe0fcb7833d92954f72ae3
menace31 <54880433+menace31@users.noreply.github.com>
2022-12-07 16:37:37 +0100

    Update mesure.py

--- mesure.py
+++ mesure.py
@@ -8,16 +8,22 @@ filelistes = glob.glob("*.csv")
 for filename in filelistes:
     file = pd.read_csv(filename)
     f = pd.DataFrame(data=file)
-    time = f["time"]
-    means = []
-    ecart_type = []
-    N = [1,2,4,8,16,32,64]
-    for i in N:
-        temps = f[f['thread']==i]['time']
-        means.append(np.mean(temps))
-        ecart_type.append(np.std(temps))
+    for j in [2]:
+        f = f[f['n_prog']==j]
+        means = []
+        ecart_type = []
+        N = [1,2,4,8,16,32,64]
+        for i in N:
+            temps = []
+            temps.append(f[f['nthreads']==i]['time1'])
+            temps.append(f[f['nthreads']==i]['time2'])
+            temps.append(f[f['nthreads']==i]['time3'])
+            temps.append(f[f['nthreads']==i]['time4'])
+            temps.append(f[f['nthreads']==i]['time5'])
+            means.append(np.mean(temps))
+            ecart_type.append(np.std(temps))
 
-    plt.errorbar(N,means,yerr=ecart_type)
+        plt.errorbar(N,means,yerr=ecart_type)
     plt.title(filename)
     plt.xlabel("nthread")
     plt.ylabel("time")
